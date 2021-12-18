8ff0cc8171f95336476a9738442f4aa32dcd124a
agrawallalit <agrawal.lalit95@gmail.com>
2021-12-18 20:00:48 +0530

    adding OpenZeppelin Contract

---
+++ handlers.js
@@ -0,0 +1,31 @@
+// create a web server
+
+// add a URL to the web server to do a transfer
+
+// let's use this web server to distribute our tokens
+
+// if there's time, let's add a docker container
+
+let contract = require("./contract.js");
+let method = require("./method.js");
+//npm install express
+const express = require("express")
+
+const app = express();
+
+app.use(express.json());
+
+const port = 8080;
+
+app.get('/symbol', async(req,res) => {
+    res.send(await contract.getSymbol())
+})
+
+app.post('/transfer', async(req, res) => {
+    let account_to = req.body.account_to;
+    let amount = req.body.amount;
+
+    res.send(await method.transferToken(account_to, amount));
+})
+
+app.listen(port, () => console.log(`listening on port ${port}...`));
--- method.js
+++ method.js
@@ -1,26 +1,18 @@
-const Web3 = require("web3")
-console.log("Hello World");
-console.log("This is Lalit");
-// this is a basic readonly contract interaction file
+// web3 ethereum talker dependency
+const Web3 = require('web3')
 
-// today's lab
-// npm install dotenv
-// this sets up my .env file
+// transaction crafting dependency
+const Tx = require('ethereumjs-tx').Transaction
+//npm install ethereumjs-tx
+
 require('dotenv').config()
 
-// let's load our environment variables
 infuraToken = process.env.INFURA_TOKEN
 contractAddress = process.env.CONTRACT_ADDRESS
 ownerAddress = process.env.OWNER_ADDRESS
-
-// set up a RPC (remote procedure call) to connect to an ethereum node
-const rpcURL = "https://ropsten.infura.io/v3/" + infuraToken;
-
-const web3 = new Web3(rpcURL);
-console.log("connected to web3");
+privateKey = Buffer.from(process.env.SUPER_SECRET_PRIVATE_KEY, 'hex')
 
 // get the ABI (interface) for our contract
-
 const abi = [
 	{
 		"inputs": [],
@@ -277,9 +269,9 @@ const abi = [
 	}
 ]
 
-// connect to our contract on ropsten
+// instantiate web3 with the infura rpc url
+const web3 = new Web3("https://ropsten.infura.io/v3/" + infuraToken);
 
-// get our contract address
 // specify our contract address 
 const address = contractAddress;
 
@@ -287,30 +279,53 @@ const address = contractAddress;
 const owner = ownerAddress;
 
 
+// connect to our contract
 const contract = new web3.eth.Contract(abi, address);
-console.log("connected to contract on ropsten");
 
-// run some of the methods in our contract (using javascript)
-
-const getTotalSupply = async() => {
-    let totSupply = await contract.methods.totalSupply().call();
-    return "total supply is: " + totSupply;
+// set up a send transaction method
+const sendTx = async(raw) => {
+    return await web3.eth.sendSignedTransaction(raw);
 }
 
-const getSymbol = async() => {
-    let symbol = await contract.methods.symbol().call();
-    return "symbol is: " + symbol;
+const transferToken = async(toAccount, amount) => {
+
+    // generate a nonce
+
+    let txCount = await web3.eth.getTransactionCount(owner);
+    console.log("tx count is " + txCount);
+
+    // generate tx data
+    const txObject = {
+        nonce: web3.utils.toHex(txCount),
+        gasLimit: web3.utils.toHex(500000),
+        gasPrice: web3.utils.toHex(web3.utils.toWei('100', 'gwei')),
+        to: contractAddress,
+        data: contract.methods.transfer(toAccount, amount).encodeABI()
+    }
+
+    const tx = new Tx(txObject, {chain: 'ropsten', hardfork: 'petersburg'})
+
+    // sign the tx
+    tx.sign(privateKey);
+
+    console.log("signed transaction with super secret private key");
+
+    // serialize the raw tx
+    const serializedTx = tx.serialize();
+    const raw = '0x' + serializedTx.toString('hex');
+
+    console.log('about to send transaction' + raw)
+    let txHash = await sendTx(raw);
+    console.log("transaction hash: " + txHash.transactionHash)
+    console.log("transaction in block: " + txHash.blockNumber)
 }
 
-const returnAllValues = async() => {
-    console.log(await getTotalSupply());
-    console.log(await getSymbol());
-}
 
-returnAllValues();
-//console.log("hello world?");
+// create a transaction to execute a method (transfer) on the contract
 
+// sign the transaction with our private key
 
-
-
-
+// broadcast the transaction
+module.exports = {transferToken}
+module.exports = {getSymbol}
+//transferToken("0xFbC8857d46223C39C48BA844c5AB0159EA3B8692", 123)
